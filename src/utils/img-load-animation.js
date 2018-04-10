d9163d774591798964030ff75cd9867ab610c5ff
LEAFERx <leaferx@outlook.com>
2018-04-10 23:02:58 +0800

    refactor img-load-animation.js

--- src/utils/img-load-animation.js
+++ src/utils/img-load-animation.js
@@ -1,21 +1,23 @@
-/* eslint-disable */
+const loadAniTemplate =
+`<div class="sk-double-bounce">
+   <div class="sk-child sk-double-bounce1"></div>
+   <div class="sk-child sk-double-bounce2"></div>
+ </div>`;
 
 export default () => {
   const imgList = document.querySelectorAll('img');
-  for (let img of imgList) {
-    if (!img.complete && img.id !== 'logo') {
+  imgList.forEach((image) => {
+    if (!image.complete && image.id !== 'logo') {
+      const img = image;
       img.style.display = 'none';
-      let loadAni = document.createElement('div');
+      const loadAni = document.createElement('div');
       loadAni.className = 'img-load-animation';
-      loadAni.innerHTML = `<div class="sk-double-bounce">
-                             <div class="sk-child sk-double-bounce1"></div>
-                             <div class="sk-child sk-double-bounce2"></div>
-                           </div>`;
+      loadAni.innerHTML = loadAniTemplate;
       img.parentNode.insertBefore(loadAni, img);
       img.onload = () => {
         loadAni.parentNode.removeChild(loadAni);
         img.style.display = 'block';
       };
     }
-  }
+  });
 };
